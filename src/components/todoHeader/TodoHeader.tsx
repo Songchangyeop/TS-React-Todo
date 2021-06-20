1dafd011709c849fdee77685eb1a3d27d60efa74
Songchangyeop <61750383+Songchangyeop@users.noreply.github.com>
2021-06-21 05:32:36 +0900

    header 수정

--- src/components/todoHeader/TodoHeader.tsx
+++ src/components/todoHeader/TodoHeader.tsx
@@ -4,7 +4,7 @@ import styles from './TodoHeader.module.css';
 function TodoHeader() {
   return (
     <div className={styles.container}>
-      <span className={styles.header}>"Todo-List"</span>
+      <span className={styles.header}>Todo-List</span>
       <span className={styles.description}>what is your next plan?</span>
     </div>
   );
