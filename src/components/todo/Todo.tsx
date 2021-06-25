b259daebdeada8838b5aa9985b5fd68f6a06c785
Songchangyeop <61750383+Songchangyeop@users.noreply.github.com>
2021-06-26 04:51:04 +0900

    각 버튼마다 Todo가 구분되어 보이게 함

--- src/components/todo/Todo.tsx
+++ src/components/todo/Todo.tsx
@@ -3,6 +3,7 @@ import TodoHeader from '../todoHeader/TodoHeader';
 import styles from './Todo.module.css';
 import TodoFormContainer from '../../containers/TodoFormContainer';
 import TodoListContainer from '../../containers/TodoListContainer';
+import { useState } from 'react';
 interface TodoItem {
   id: number;
   text: string;
@@ -14,6 +15,13 @@ interface TodoProps {
 }
 
 function Todo({ todos }: TodoProps) {
+  const [selected, setSelected] = useState('Doing');
+  const handleSelected = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setSelected(e.currentTarget.value);
+  };
+
+  let doingTodo = todos.filter((todo) => todo.done === false);
+  let completedTodo = todos.filter((todo) => todo.done === true);
   return (
     <div className={styles.Todo}>
       <header className={styles.header}>
@@ -22,10 +30,41 @@ function Todo({ todos }: TodoProps) {
       <section className={styles.form}>
         <TodoFormContainer />
       </section>
+      <div className={styles.buttons}>
+        <button
+          className={styles.button}
+          value="Doing"
+          onClick={handleSelected}
+        >
+          Doing
+        </button>
+        <button
+          className={styles.button}
+          value="Completed"
+          onClick={handleSelected}
+        >
+          Completed
+        </button>
+        <button
+          className={styles.button}
+          value="ViewAll"
+          onClick={handleSelected}
+        >
+          View All
+        </button>
+      </div>
+
       <ul className={styles.list}>
-        {todos.map((todo) => (
-          <TodoListContainer todo={todo} key={todo.id} />
-        ))}
+        {selected === 'ViewAll' &&
+          todos.map((todo) => <TodoListContainer todo={todo} key={todo.id} />)}
+        {selected === 'Doing' &&
+          doingTodo.map((todo) => (
+            <TodoListContainer todo={todo} key={todo.id} />
+          ))}
+        {selected === 'Completed' &&
+          completedTodo.map((todo) => (
+            <TodoListContainer todo={todo} key={todo.id} />
+          ))}
       </ul>
     </div>
   );
